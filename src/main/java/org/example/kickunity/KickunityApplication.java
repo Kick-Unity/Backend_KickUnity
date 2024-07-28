8f5522f262c580284f5ef27159ed2b973a53ee14
장영재 <urinaner@naver.com>
2024-07-28 13:56:55 +0900

    init

---
+++ src/main/java/org/example/kickunity/KickunityApplication.java
@@ -0,0 +1,13 @@
+package org.example.kickunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KickunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/kickunity/KickunityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kickunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
