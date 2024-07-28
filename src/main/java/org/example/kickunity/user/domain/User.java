b26b06a11b13a4b39af2f7ef0996ce225a04b277
장영재 <urinaner@naver.com>
2024-07-28 17:21:58 +0900

    feat: domain

--- src/main/java/org/example/kickunity/user/domain/User.java
+++ src/main/java/org/example/kickunity/user/domain/User.java
@@ -1,9 +1,65 @@
 package org.example.kickunity.user.domain;
 
-public class User {
-    private String name;
-    private String email;
-    private String phone_number;
-    private long score;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.example.kickunity.global.entity.BaseEntity;
 
+
+import java.util.Objects;
+
+@Entity
+public class User extends BaseEntity {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private String email;
+
+    protected User() {
+    }
+
+    public User(final String email) {
+        this(null, email);
+    }
+
+    public User(final Long id, final String email) {
+        this.id = id;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final User member = (User) o;
+        return Objects.equals(id, member.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
