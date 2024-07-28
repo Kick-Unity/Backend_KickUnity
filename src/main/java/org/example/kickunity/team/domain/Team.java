7aa5fb2e77b0f027f300e50080319aa53916643c
장영재 <urinaner@naver.com>
2024-07-28 14:23:19 +0900

    feat: domain

---
+++ src/main/java/org/example/kickunity/team/domain/Team.java
@@ -0,0 +1,6 @@
+package org.example.kickunity.team.domain;
+
+public class Team {
+    private String name;
+    private String score;
+}
---
+++ src/main/java/org/example/kickunity/user/domain/User.java
@@ -0,0 +1,9 @@
+package org.example.kickunity.user.domain;
+
+public class User {
+    private String name;
+    private String email;
+    private String phone_number;
+    private long score;
+
+}
